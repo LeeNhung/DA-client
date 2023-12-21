df3e32046dc143c42bd6ab939996eefba2d65c06
LeeNhung <93263032+LeeNhung@users.noreply.github.com>
2023-12-21 22:35:27 +0700

    Update BlogController.java

--- src/main/java/com/example/controller/BlogController.java
+++ src/main/java/com/example/controller/BlogController.java
@@ -27,7 +27,6 @@ import jakarta.servlet.http.HttpSession;
 public class BlogController {
 	private RestTemplate rest=new RestTemplate();
 	private String url="https://server-production-2465.up.railway.app";
-	//private String url="https://da-server2-production.up.railway.app";
 	@GetMapping
 	private String getListBlog(HttpSession session,
 			@RequestParam(name="pageNum", defaultValue = "1", required = false) int pageNum,
